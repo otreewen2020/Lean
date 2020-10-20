3991fcd6b5a70187702c185f892f7cd875c5d0f3
Adalyat Nazirov <aenazirov@gmail.com>
2020-10-20 22:15:08 +0300

    comparison operators and tests

--- Common/Extensions.cs
+++ Common/Extensions.cs
@@ -2337,5 +2337,18 @@ namespace QuantConnect
 
             return new OptionChainUniverse(optionChain, settings, algorithm.LiveMode);
         }
+
+        /// <summary>
+        /// Compares two values using given operator
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="op">Comparison operator</param>
+        /// <param name="arg1">The first value</param>
+        /// <param name="arg2">The second value</param>
+        /// <returns>Returns true if its left-hand operand meets the operator value to its right-hand operand, false otherwise</returns>
+        public static bool Compare<T>(this ComparisonOperatorTypes op, T arg1, T arg2) where T : IComparable
+        {
+            return ComparisonOperator.Compare(op, arg1, arg2);
+        }
     }
 }
